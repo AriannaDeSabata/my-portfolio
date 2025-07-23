0d68e86eb59df2c4e325a1b28c02672a03c7336c
AriannaDeSabata <desabatarianna@gmail.com>
2025-07-23 17:12:14 +0200

    update scrollDown

--- src/components/works/ComponentWorks.jsx
+++ src/components/works/ComponentWorks.jsx
@@ -15,7 +15,7 @@ export default function ComponentWorks() {
     const element = document.getElementById('projectsInfo')
     const y = element.getBoundingClientRect().top + window.scrollY
     window.scrollTo({
-      top: y -100 ,
+      top: y  ,
       behavior: 'smooth'
     })
   }
